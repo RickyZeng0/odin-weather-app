4104532dbc485657b3a963565bd9d7c1a2609ce2
RickyZeng <121326846+RickyZeng0@users.noreply.github.com>
2026-01-14 11:45:19 +0800

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import hi from "./test.js";
+import "./style.css";
+
+console.log(hi);
+console.log("Hello World again !");
---
+++ webpack.common.js
@@ -0,0 +1,37 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      }
+    ],
+  },
+};
+
+/*
+git branch gh-pages
+git checkout gh-pages && git merge main --no-edit
+npm run build
+git add dist -f && git commit -m "Deployment commit"
+git subtree push --prefix dist origin gh-pages
+git checkout main
+*/
